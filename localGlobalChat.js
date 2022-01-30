625fb6304d1868b8bf3e1111dad17c07218ab76d
Mixer <51480483+shizotoaster@users.noreply.github.com>
2022-01-30 17:55:25 +0600

    The script is fully rewritten. beta release

--- localGlobalChat.js
+++ localGlobalChat.js
@@ -7,6 +7,7 @@
 //
 //Allows to use local and global chat in your game!
 //Author: qIMIXERIp vk.com/svtlng
+//Version 2.0
 
 //config
 let radius = 10 //Radius of local message (in blocks)
@@ -15,39 +16,58 @@ let globalPrefix = '[G]' //Prefix that marks message as global
 
 let headChat = true //Displays messages above player's head
 	let smallMessage = 10 //Amount of symbols that small message has
-	let headDelaySmall = 1 //Message's delay above player's head for small messages (in seconds)
+
+let useMuteSystem = true //Set to true if you are using this script witn MixerAPI
 //yes, I could make it in JSON but I don't want to
 
-//code
-mc.listen("onChat", function(player, msg){ //listens to chat message event
-	let name = player.realName //Getting player's real name
-	let pos = player.pos //Getting player's position
-	let x = pos.x //Pos x
-	let y = pos.y //Pos y
-	let z = pos.z //Pos z
-	let message = msg
-	if(headChat ==  true){
-		if(message[0] == globalSymbol){ //checks for global chat
-			let globalMessage = message.replace('!','')
-			player.rename(name + "\n>> " + globalMessage) //renames player
+function setHeadMsg(mes) {
+	//player.rename(name + "\n>> " + message)
+	player.rename(`${name}\n>> ${mes}`)
+	if(mes.length >= smallMessage){
+		setTimeout(setRname, mes.length * 500)
+	}else{
+		setTimeout(setRname, mes.length * 1000)
+	}
+}
+
+function sendMsgToChat(type, mssg, name, x, y, z){
+	switch(type){
+		case 0:
+			mc.runcmd(`tellraw @a[x=${x},y=${y},z=${z},r=${radius}] {"rawtext":[{"text":"<${name}> ${mssg}"}]}`)
+			break;
+		case 1:
+			mc.runcmd(`tellraw @a {"rawtext":[{"text":"${globalPrefix} <${name}> ${mssg}"}]}`)
+			break;
+	}
+}
+
+mc.listen('onChat', function(player, msg){
+	let isMuted = player.hasTag(`is_muted`)
+	let rname = player.realName
+	let x = player.pos.x
+	let y = player.pos.y
+	let z = player.pos.z
+	let muteMsg
+	let isGlobal = msg[0] == globalSymbol
+	if (isMuted && useMuteSystem){
+		return false
+		muteMsg = true
+	}
+	if (headChat && !muteMsg) {
+		if (isGlobal){
+			msg = msg.replace('!','')
+			setHeadMsg(msg)
 		}else{
-			player.rename(name + "\n>> " + message) //renames player
-		}
-		function setRname(){
-			player.rename(name);
-		}
-		if(message.length >= smallMessage){
-			setTimeout(setRname, message.length * 500) //return player's name to normal
-		}else{
-			setTimeout(setRname, headDelaySmall * 1000) //return player's name to normal
+			setHeadMsg(msg)
 		}
 	}
-	if(message[0] == globalSymbol){ //If message has globalSymbol, then send global message
-		let globalMessage = message.replace('!','')
-		mc.runcmd('tellraw @a[x=' + x + ',y=' + y + ',z=' + z + '] {"rawtext":[{"text":"' + globalPrefix + ' <' + name + '> ' + globalMessage + '"}]}')
-		return false
-	}else{ //Else send local message
-		mc.runcmd('tellraw @a[x=' + x + ',y=' + y + ',z=' + z + ',r=' + radius + '] {"rawtext":[{"text":"' + '<' + name + '> ' + message + '"}]}')
-		return false
+	if (!muteMsg){
+		if (isGlobal){
+			sendMsgToChat(1, msg)
+			return false
+		}else{
+			sendMsgToChat(0, msg)
+			return false
+		}
 	}
-});
+})
